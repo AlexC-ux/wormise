d43047f7ced263615f46c1ff80a9e54ebc5ecd18
AlexC-ux <eunv2004@gmail.com>
2024-08-28 22:12:22 +0300

    feat: new build

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,7 +1,8 @@
+type CallbackReturnType = Object | void;
 type CallbackType<ParamsType, ReturnType> = (params: ParamsType) => ReturnType;
 export type WorkerData<ParamsType> = {
     params: ParamsType;
     cb: string;
 };
-export default function wormise<ParamsType, ReturnType extends Object>(params: ParamsType, executedFunction: CallbackType<ParamsType, ReturnType>): Promise<ReturnType>;
+export default function wormise<ParamsType, ReturnType extends CallbackReturnType>(params: ParamsType, executedFunction: CallbackType<ParamsType, ReturnType>): Promise<ReturnType>;
 export {};
